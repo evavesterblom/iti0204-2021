f89aff78d47a3b2f593d785300b701f8fc02b507
Evgneia Vesterblom <evgenia.udod@ttu.ee>
2021-09-03 20:43:35 +0300

    Time estimation push B part 42

--- AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
+++ AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
@@ -22,7 +22,7 @@ public class AL01B {
         // x = 21.47 * 1.618 ˇ(n-45) sekundit - kui on sisend 45.
         // x = baseTime * expBase ˇ(n-exponent)
 
-        int base = 33;
+        int base = 42;
         double expBase = 1.618;
 
         Instant before = Instant.now();
