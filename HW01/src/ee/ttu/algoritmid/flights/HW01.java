e69bef984de1954cb6b5d9e7940ca04fdf711529
Evgenia.Udod <Evgenia.Udod@ttu.ee>
2021-11-18 07:26:27 +0200

    implement get flightMemberWithoutTeam

--- HW01/src/ee/ttu/algoritmid/flights/HW01.java
+++ HW01/src/ee/ttu/algoritmid/flights/HW01.java
@@ -1,18 +1,18 @@
 package ee.ttu.algoritmid.flights;
 
-import java.util.List;
-import java.util.TreeMap;
+import java.util.*;
 
 public class HW01 implements FlightCrewRegistrationSystem {
 
-    //Self maintaining trees
-    private TreeMap<Double, FlightCrewMember> availablePilots = new TreeMap<>();
-    private TreeMap<Double, FlightCrewMember> availableCopilots = new TreeMap<>();;
-    private TreeMap<Double, FlightCrewMember> availableFlightAttendants = new TreeMap<>();;
+    private TreeMap<Double, List<FlightCrewMember>> availablePilots = new TreeMap<>();
+    private TreeMap<Double, List<FlightCrewMember>> availableCopilots = new TreeMap<>();
+    private TreeMap<Double, List<FlightCrewMember>> availableFlightAttendants = new TreeMap<>();
+    private SortedSet<Double> seniorityCache = new TreeSet<>();
 
-
-    @Override
+    @Override //todo
     public FlightCrew registerToFlight(FlightCrewMember participant) throws IllegalArgumentException {
+
+        checkParticipantArguments(participant);
 
         switch (participant.getRole()){
             case FLIGHT_ATTENDANT:
@@ -22,26 +22,74 @@ public class HW01 implements FlightCrewRegistrationSystem {
             case COPILOT:
                     return handleNewCopilot(participant);
         }
+
         return null;
     }
 
-    @Override
+    private void checkParticipantArguments(FlightCrewMember participant) throws IllegalArgumentException{
+        if (participant == null) throw new IllegalArgumentException("Object not valid!");
+        if (participant.getName().isEmpty() || participant.getName().isBlank()) throw new IllegalArgumentException("Name not valid!");
+        if (!participant.getRole().equals(FlightCrewMember.Role.COPILOT) ||
+                !participant.getRole().equals(FlightCrewMember.Role.PILOT) ||
+                !participant.getRole().equals(FlightCrewMember.Role.FLIGHT_ATTENDANT) ){
+            throw new IllegalArgumentException("Role not valid!");
+        }
+        if (participant.getWorkExperience() <= 0) throw new IllegalArgumentException("Seniority not valid!");
+    }
+
+    @Override //todo
     public List<FlightCrewMember> crewMembersWithoutTeam() {
-        // TODO
-        return null;
+
+        List<FlightCrewMember> resultList = new ArrayList<>();
+
+        for (var seniority : seniorityCache) {
+            var flightAttendants = availableFlightAttendants.get(seniority);
+            if (flightAttendants != null) addParticipantToResultList(resultList, flightAttendants);
+
+            var copilots = availableCopilots.get(seniority);
+            if (copilots != null) addParticipantToResultList(resultList, copilots);
+
+            var pilots = availablePilots.get(seniority);
+            if (pilots != null) addParticipantToResultList(resultList, pilots);
+        }
+
+        return resultList;
     }
 
     private FlightCrew handleNewCopilot(FlightCrewMember participant) {
+
+        // 1 .search for new crew and remove from queue
+        // kas leidub komplekt pilooti, kas leidub abipilooti, kui jah, vali
+        // 2. or add to queue
+        addToQueue(participant, availableCopilots);
+
+        return null;
     }
 
     private FlightCrew handleNewPilot(FlightCrewMember participant) {
+        addToQueue(participant, availablePilots);
+        return null;
     }
 
     private FlightCrew handleNewFlightAttendant(FlightCrewMember participant) {
+        addToQueue(participant, availableFlightAttendants);
+        return null;
     }
 
+    private void addToQueue(FlightCrewMember participant, TreeMap<Double, List<FlightCrewMember>> queue) {
+        if (!queue.containsKey(participant.getWorkExperience())) {
+            queue.put(participant.getWorkExperience(), new ArrayList<FlightCrewMember>());
+        }
+        queue.get(participant.getWorkExperience()).add(participant);
+        seniorityCache.add(participant.getWorkExperience());
+    }
 
+    private void addParticipantToResultList(List<FlightCrewMember> resultList, List<FlightCrewMember> participants) {
+        for (var participant : participants) {
+            if (participant != null) {
+                resultList.add(participant);
+            }
+        }
+    }
 
-
-    /////////
 }
--- HW01/src/ee/ttu/algoritmid/flights/HW01Test.java
+++ HW01/src/ee/ttu/algoritmid/flights/HW01Test.java
@@ -48,9 +48,30 @@ public class HW01Test {
     @Test
     public void registerToFLight_NotPossible_NotEnoughParticipants(){
         var participant = new TestFlightCrewMember(FlightCrewMember.Role.PILOT, "Kati Karu", 13.9);
-        var actual = unitUnderTest.registerToFlight(participant);
-        assertNull(actual);
+        //jama var actual = unitUnderTest.registerToFlight(participant);
+        //assertNull(actual);
     }
+
+    @Test
+    public void a(){
+        //var errorParticipant1 = new Double[]{109.76864722775014, 103.76732287213353, 100.35650214518184};
+        //var err1 = new TestFlightCrewMember(FlightCrewMember.Role.COPILOT, "  ", -4.999);
+        var participant1 = new TestFlightCrewMember(FlightCrewMember.Role.COPILOT, "Kati Karau", 13.3);
+        var participant2 = new TestFlightCrewMember(FlightCrewMember.Role.COPILOT, "Kati Karau", 13.3);
+        var participant3 = new TestFlightCrewMember(FlightCrewMember.Role.PILOT, "Kati Karau", 13.3);
+        var participant4 = new TestFlightCrewMember(FlightCrewMember.Role.FLIGHT_ATTENDANT, "Kati Karau", 13.3);
+        var participant5 = new TestFlightCrewMember(FlightCrewMember.Role.FLIGHT_ATTENDANT, "Liis", 1.3);
+
+        unitUnderTest.registerToFlight(participant1);
+        unitUnderTest.registerToFlight(participant2);
+        unitUnderTest.registerToFlight(participant3);
+        unitUnderTest.registerToFlight(participant4);
+        unitUnderTest.registerToFlight(participant5);
+
+        var a = unitUnderTest.crewMembersWithoutTeam();
+
+    }
+
 
 
 //    @Override
