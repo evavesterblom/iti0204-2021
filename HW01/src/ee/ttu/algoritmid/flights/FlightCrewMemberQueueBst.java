6000c2b23101bea3d5dffda50db71874aa2a73f8
Evgenia.Udod <Evgenia.Udod@ttu.ee>
2021-11-27 00:58:17 +0200

    add BST balancer

--- HW01/src/ee/ttu/algoritmid/flights/FlightCrewMemberQueueBst.java
+++ HW01/src/ee/ttu/algoritmid/flights/FlightCrewMemberQueueBst.java
@@ -1,7 +1,5 @@
 package ee.ttu.algoritmid.flights;
 
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +7,7 @@ import java.util.List;
 class Node {
     double key;
     Node left, right;
+    int height;
 
     ArrayList<FlightCrewMember> availableCoPilots = new ArrayList<>();
     ArrayList<FlightCrewMember> availablePilots = new ArrayList<>();
@@ -17,12 +16,19 @@ class Node {
 
     Node(double k) {
         key = k;
+        height = 1;
     }
 }
 
 class BSTTree {
 
     private Node root;
+
+    private boolean balance;
+
+    BSTTree(boolean doBalancing){
+        balance = doBalancing;
+    }
 
 
     public Node getRoot(){
@@ -43,7 +49,7 @@ class BSTTree {
             return;
         }
 
-        put(root, key);
+        root = put(root, key);
     }
 
     public void delete(double key){
@@ -61,6 +67,7 @@ class BSTTree {
         treeWalkInorderRange(root, list, fromKey, toKey, isReversed);
         return list;
     }
+
 
 
     private void treeWalkInorderRange(Node node, ArrayList<Node> list, double fromKey, double toKey, boolean isReversed){
@@ -101,6 +108,22 @@ class BSTTree {
         if (key > node.key) node.right = put(node.right, key);
         if (key == node.key) return node;
 
+        //Balance BST
+        if (balance) {
+            node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+            var balance = getBalance(node);
+
+            if (balance > 1 && key < node.left.key) return rotateRight(node); //Left-left rotation
+            if (balance < -1 && key > node.right.key) return rotateLeft(node); //Right-right rotation
+            if (balance > 1 && key > node.left.key){
+                node.left = rotateLeft(node.left);
+                return rotateRight(node);
+            }
+            if (balance < -1 && key < node.right.key){
+                node.right = rotateRight(node.right);
+                return rotateLeft(node);
+            }
+        }
         return node;
     }
 
@@ -130,8 +153,25 @@ class BSTTree {
 
             // successor delete and add right subtree to node right
             node.right = delete(node.right, node.key);
-
         }
+
+        //Balance BST
+        if (balance) {
+            node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+            var balance = getBalance(node);
+
+            if (balance > 1 && getBalance(node.left) >= 0) return rotateRight(node); //Left-left rotation
+            if (balance < -1 && getBalance(node.right) <= 0) return rotateLeft(node); //Right-right rotation
+            if (balance > 1 && getBalance(node.left) < 0){ //Left-right rotation
+                node.left = rotateLeft(node.left);
+                return rotateRight(node);
+            }
+            if (balance < -1 && getBalance(node.right) > 0){//Right-left rotation
+                node.right = rotateRight(node.right);
+                return rotateLeft(node);
+            }
+        }
+
         return node;
     }
 
@@ -142,11 +182,48 @@ class BSTTree {
         }
         return node;
     }
+
+    private int getHeight(Node node){
+        if (node == null) return 0;
+        return node.height;
+    }
+
+    private int getBalance(Node node){
+        if (node == null) return 0;
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    private Node rotateRight(Node a){
+
+        Node newRoot = a.left;
+        Node newRootLeftChild = newRoot.right;
+
+        newRoot.right = a;
+        a.left = newRootLeftChild;
+
+        a.height = Math.max(getHeight(a.right), getHeight(a.left)) + 1;
+        newRoot.height  = Math.max(getHeight(newRoot.right), getHeight(newRoot.left)) + 1;
+
+        return newRoot;
+    }
+
+    private Node rotateLeft(Node a){
+        Node newRoot = a.right;
+        Node newRootLeftChild = newRoot.left;
+
+        newRoot.left = a;
+        a.right = newRootLeftChild;
+
+        a.height = Math.max(getHeight(a.right), getHeight(a.left)) + 1;
+        newRoot.height  = Math.max(getHeight(newRoot.right), getHeight(newRoot.left)) + 1;
+
+        return newRoot;
+    }
 }
 
 public class FlightCrewMemberQueueBst{
 
-    private final BSTTree availableMembersQueue = new BSTTree();
+    private final BSTTree availableMembersQueue = new BSTTree(true);
 
     public void removeFromQueue(FlightCrewMember participant){
         var seniority = participant.getWorkExperience();
@@ -232,24 +309,75 @@ public class FlightCrewMemberQueueBst{
 
     //TREE CREATE
     public static void main(String[] args) {
-        BSTTree tree = new BSTTree();
+        BSTTree tree = new BSTTree(true);
 
+        //Left rotate
+        /*
         tree.put(5.0);
-        tree.get(5.0).testList.add(0, "viis");
-
+        tree.get(5.0).testList.add(0, "5");
         tree.put(3.0);
-        tree.get(3.0).testList.add(0, "kolm-tst");
-        tree.put(9.0);
-        tree.get(9.0).testList.add(0, "yheksa-tst");
-        tree.put(7.0);
-        tree.get(7.0).testList.add(0, "seitse-tst");
-        tree.put(19.0);
-        tree.get(19.0).testList.add(0, "yheksateist-tst");
+        tree.get(3.0).testList.add(0, "3-tst");
         tree.put(6.0);
-        tree.get(6.0).testList.add(0, "kuus-tst");
-        tree.put(20.0);
-        tree.get(20.0).testList.add(0, "kakskend-tst");
-        tree.delete(20.0);
+        tree.get(6.0).testList.add(0, "6-tst");
+        tree.put(2.0);
+        tree.get(2.0).testList.add(0, "2-tst");
+        tree.put(3.5);
+        tree.get(3.5).testList.add(0, "3.5-tst");
+        tree.put(1.5);
+        tree.get(1.5).testList.add(0, "1.5-tst");
+         */
+
+        /// Right rotate
+        /*
+        tree.put(5.5);
+        tree.get(5.5).testList.add(0, "5.5-tst");
+        tree.put(6.5);
+        tree.get(6.5).testList.add(0, "6.5-tst");
+        tree.put(7.5);
+        tree.get(7.5).testList.add(0, "7.5-tst");
+        */
+
+        //Left right rotate
+/*
+        tree.put(9.0);
+        tree.get(9.0).testList.add(0, "9");
+        tree.put(4.0);
+        tree.get(4.0).testList.add(0, "4");
+        tree.put(7.0);
+        tree.get(7.0).testList.add(0, "7");
+
+ */
+
+
+        //Right left rotate
+        /*
+        tree.put(8.0);
+        tree.get(8.0).testList.add(0, "8");
+        tree.put(10.0);
+        tree.get(10.0).testList.add(0, "10");
+        tree.put(9.0);
+        tree.get(9.0).testList.add(0, "9");
+         */
+
+        tree.put(0.1);
+        tree.get(0.1).testList.add(0, "0.1");
+        tree.put(3.9);
+        tree.get(3.9).testList.add(0, "3.9");
+        tree.put(2.0);
+        tree.get(2.0).testList.add(0, "2.0");
+        tree.put(6.6);
+        tree.get(6.6).testList.add(0, "6.6");
+        tree.put(9.0);
+        tree.get(9.0).testList.add(0, "9.0");
+
+        //tree.delete(1.0);
+
+
+        //tree.put(20.0);
+        //tree.get(20.0).testList.add(0, "kakskend-tst");
+        //tree.delete(20.0);
+
+        var stopHere = "stop";
 
         var treeAsc= tree.treeWalkInorder(false);
         var treeDesc= tree.treeWalkInorder(true);
