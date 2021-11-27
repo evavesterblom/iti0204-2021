ef5d016cc341738c607fbb6a0eafd7df1360c64c
Evgenia.Udod <Evgenia.Udod@ttu.ee>
2021-11-27 17:43:09 +0200

    try breaking out of stamp sorting loop early.

--- AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
+++ AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
@@ -1,7 +1,6 @@
 package ee.ttu.algoritmid.interestingstamps;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class InterestingStamps {
@@ -9,15 +8,16 @@ public class InterestingStamps {
 
         if (stampOptions.size() == 0) throw new IllegalArgumentException("Illegal input");
 
-        List<Integer> interesingStamps = new ArrayList<>();
+        List<Integer> interestingStamps = new ArrayList<>();
         List<Integer> boringStamps = new ArrayList<>();
 
         for (var stamp : stampOptions) {
+            if (stamp > sum) break;
             if(stamp == 1 || Math.floorMod(stamp, 10) == 0) boringStamps.add(stamp);
-            else {interesingStamps.add(stamp);}
+            else {interestingStamps.add(stamp);}
         }
 
-        //Collections.sort(interesingStamps, Collections.reverseOrder());
+        //Collections.sort(interestingStamps, Collections.reverseOrder());
         //Collections.sort(boringStamps);
 
         var M = new int[sum+1];
@@ -27,18 +27,16 @@ public class InterestingStamps {
 
             M[i] = Integer.MAX_VALUE;
 
-            for (int s = 0; s < interesingStamps.size(); s++){
-                var stamp = interesingStamps.get(s);
-                if (i >= stamp && M[i] > M[i-stamp] + 1){
-                    M[i] = M[i-stamp] + 1;
+            for (Integer stamp : interestingStamps) {
+                if (i >= stamp && M[i] > M[i - stamp] + 1) {
+                    M[i] = M[i - stamp] + 1;
                     V[i] = stamp;
                 }
             }
 
-            for (int s = 0; s < boringStamps.size(); s++){
-                var stamp = boringStamps.get(s);
-                if (i >= stamp && M[i] > M[i-stamp] + 1){
-                    M[i] = M[i-stamp] + 1;
+            for (Integer stamp : boringStamps) {
+                if (i >= stamp && M[i] > M[i - stamp] + 1) {
+                    M[i] = M[i - stamp] + 1;
                     V[i] = stamp;
                 }
             }
@@ -49,7 +47,7 @@ public class InterestingStamps {
 
     private static List<Integer> printStamps(int n, int[] V){
 
-        List<Integer> result = new ArrayList<Integer>();
+        List<Integer> result = new ArrayList<>();
         while (n > 0){
             result.add(V[n]);
             n = n - V[n];
@@ -59,7 +57,7 @@ public class InterestingStamps {
 
     public static void main(String[] args) {
 
-        var sum = 100;
+        var sum = 100000;
         var stampOptions = new ArrayList<Integer>();
         stampOptions.add(1);
         stampOptions.add(10);
