f4b6f0f957a5efbb99191b73d8e14dfec0608aa8
Evgenia.Udod <Evgenia.Udod@ttu.ee>
2021-11-28 12:15:12 +0200

    init HW02

---
+++ HW02/src/ee/ttu/algoritmid/trampoline/HW02.java
@@ -0,0 +1,10 @@
+package ee.ttu.algoritmid.trampoline;
+
+public class HW02 implements TrampolineCenter {
+
+    @Override
+    public Result play(Trampoline[][] map) {
+        // TODO
+        return null;
+    }
+}
